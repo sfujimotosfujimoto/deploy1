634f513da632fa0313d97ff3ab03e8ee28b99959
sfujimoto <unicoshun@gmail.com>
2016-10-16 12:05:27 +0900

    change src

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+
+
+import { RecipeService } from './recipes/recipe.service';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { routing } from "./app.routing";
+import {CoreModule} from "./core.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    HeaderComponent
+
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    HttpModule,
+    routing,
+    CoreModule
   ],
-  providers: [],
+  providers: [RecipeService, ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,69 @@
+import {Injectable, EventEmitter} from '@angular/core';
+import { Recipe } from './recipe';
+import { Ingredient } from '../shared/ingredient';
+import {Headers, Http, Response} from "@angular/http";
+import 'rxjs/Rx';
+
+
+@Injectable()
+export class RecipeService {
+  recipesChanged = new EventEmitter<Recipe[]>();
+  private recipes: Recipe[] = [
+    new Recipe('Curry', 'Very tasty', '../../assets/curry.jpg', [
+      new Ingredient('French Fries', 2),
+      new Ingredient('Pork Fries', 1)
+    ]),
+    new Recipe('Sushi', 'Good!', '../../assets/sushi.jpg', [
+      new Ingredient('Squid', 4),
+      new Ingredient('Rice', 5)
+    ]),
+    new Recipe('Takoyaki', 'The sauce!', '../../assets/takoyaki.jpg', [
+      new Ingredient('Octopus', 2),
+      new Ingredient('Flour', 3)
+    ]),
+    new Recipe('Sukiyaki', 'Very good!', '../../assets/sukiyaki.jpg', [
+      new Ingredient('Beef', 2),
+      new Ingredient('Sauce', 3)
+    ])
+  ];
+
+  constructor(private http: Http) { }
+  getRecipes() {
+    return this.recipes;
+  }
+
+  getRecipe(id: number) {
+    return this.recipes[id];
+  }
+
+  deleteRecipe(recipe: Recipe) {
+    this.recipes.splice(this.recipes.indexOf(recipe), 1)
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+  }
+
+  editRecipe(oldRecipe: Recipe, newRecipe: Recipe) {
+    this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
+  }
+
+  storeData() {
+    const body = JSON.stringify(this.recipes);
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    return this.http.put('https://recipe-book-dc64a.firebaseio.com/recipes.json', body, {headers: headers});
+  }
+
+  fetchData() {
+    return this.http.get('https://recipe-book-dc64a.firebaseio.com/recipes.json')
+      .map((response: Response) => response.json())
+      .subscribe(
+        (data: Recipe[]) => {
+          this.recipes = data;
+          this.recipesChanged.emit(this.recipes);
+        }
+      );
+  }
+}
